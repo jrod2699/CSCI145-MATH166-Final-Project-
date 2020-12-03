a459e51eb25f1bd54964fa7dba83743e8d57293d
jrod2699 <jrodriguez261099@gmail.com>
2020-12-03 00:50:42 -0800

    Added sentiment analysis to all tweets, combined all data files into single file

---
+++ code/combine_data.py
@@ -0,0 +1,15 @@
+import os
+import glob
+import pandas as pd
+
+os.chdir("/home/sgba2018/CSCI145-MATH166-Final-Project-/data")
+
+extension = 'csv'
+
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+
+#export csv
+combined_csv.to_csv("combined_data.csv", index=False, encoding='utf-8-sig')
